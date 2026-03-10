8041e4943d113672ed2ce6c772d4235dfffe6cbd
s34050 <a@>
2026-03-10 11:45:41 +0100

    logic for even number changed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ int number = int.Parse(Console.ReadLine());
 
 if (number % 2 == 0)
 {
-    Console.WriteLine("The number is even.");
+    Console.WriteLine("Please enter an odd number. This number is even.");
 }
 else
 {
